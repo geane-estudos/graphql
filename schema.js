d598cd9181c66ee78f81eb88521d69e21adbc604
Geane Moraes Ramos <geane.ramos@contavoltz.com>
2023-12-01 07:48:42 -0300

    chore: dados relacionados

--- schema.js
+++ schema.js
@@ -3,22 +3,28 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]! #array of string
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
     type Query { #not optional - entry points for users
         reviews: [Review]
         review(id: ID!): Review
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
     }
 `;
 
